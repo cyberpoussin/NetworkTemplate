23e77fcfc5d1b687c72775177451442b2a32ed02
Adrien <adrnsmn@gmail.com>
2021-10-12 21:01:40 +1200

    Initial Commit

---
+++ NetworkTemplate/NetworkTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkTemplateApp.swift
+//  NetworkTemplate
+//
+//  Created by Admin on 12/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct NetworkTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
